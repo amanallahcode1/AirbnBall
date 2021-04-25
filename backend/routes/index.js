2684d63fe794d43f33153e24d2aec991c1193aa3
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-04-25 03:47:22 +0100

    Finishing up user auth middleware

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// All URL's of the routes will be prefixed with /api
 
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
