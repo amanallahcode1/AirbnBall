26143b654965bf79e179aa4f1f49ca8fcd85ecf0
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-04-25 02:35:18 +0100

    set up app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,36 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if (!isProduction) {
+    // only enable cors in development
+    app.use(cors())
+}
+// helmet helps set a variety of headers to better secure
+// the app
+
+app.use(helmet({
+    contentSecurityPolicy: false
+}));
+// Set _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie:{
+            secure: isProduction,
+            sameSite: isProduction && 'Lax',
+            httpOnly: true,
+        },
+    })
+);
