97872b10c7d369b815578bd358c5cf4f52bebf1c
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-04-26 01:54:57 +0100

    splash page almost done

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -5,22 +5,13 @@ function Banner() {
     return (
         <div className='banner'>
           <div id='splash-form'>
-              <h2>Search for courts that are
-                  available for you and your friends to ball in.
-                  Put that work in!
+              <h2>Book a court. Let's ball.
               </h2>
             <form>
                 <div class="user-box">
-                    <input type="text" name="" required="" placeholder="Try Los Angeles..."/>
+                    <input type="text" name="" required="" placeholder="Try 'Los Angeles'..."/>
                     <label>LOCATION</label>
                 </div>
-               
-            <a href="#">
-                <button class='form-button'>
-                    Submit
-                </button>
-            
-            </a>
             </form>
           </div>
         </div>
