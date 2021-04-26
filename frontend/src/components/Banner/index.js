f0c5f8fb51371bfea7acdf6e663ce9d04132450d
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-04-26 02:44:10 +0100

    Implemented react-date-picker

--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 import './banner.css';
 
 function Banner() {
+
+const [startDate, setStartDate] = useState(new Date());
+const [endDate, setEndDate] = useState(new Date());
     return (
         <div className='banner'>
           <div id='splash-form'>
               <h2>Book a court. Let's ball.
               </h2>
             <form>
-                <div class="user-box">
+                <div className="user-box">
                     <input type="text" name="" required="" placeholder="Try 'Los Angeles'..."/>
                     <label>LOCATION</label>
+                </div>
+                <div className="date-box">
+                    <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+                    <DatePicker selected={endDate} onChange={date => setEndDate(date)} />
+                    <label>FIND DATES</label>
+
                 </div>
             </form>
           </div>
