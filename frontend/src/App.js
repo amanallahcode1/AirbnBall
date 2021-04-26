bcf2856ac10c6b29939a1539953832c61b8a18c3
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-04-26 01:32:26 +0100

    almost done with splash form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Home from './components/Home';
 import NavBar from './components/NavBar'
+import './App.css'
 function App() {
 
   return (
--- frontend/src/components/Banner/index.js
+++ frontend/src/components/Banner/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import './banner.css';
+
 function Banner() {
     return (
-        <div>
+        <div className='banner'>
+          <div id='splash-form'>
+              <h2>Search for courts that are
+                  available for you and your friends to ball in.
+                  Put that work in!
+              </h2>
+            <form>
+                <div class="user-box">
+                    <input type="text" name="" required="" placeholder="Try Los Angeles..."/>
+                    <label>LOCATION</label>
+                </div>
+               
+            <a href="#">
+                <button class='form-button'>
+                    Submit
+                </button>
             
+            </a>
+            </form>
+          </div>
         </div>
     )
 }
