ece4ab50a077914421d20bc9c38b89950287a96e
Shahene Chaouachi <achaouac@mail.ccsf.edu>
2021-05-18 07:23:47 +0100

    error showing up with dates for sequelize

--- backend/db/seeders/20210518061414-booking-data.js
+++ backend/db/seeders/20210518061414-booking-data.js
@@ -9,21 +9,24 @@ module.exports = {
               endDate: "2021-5-21",
               userId: 1,
               spotId: 2,
-                
+              createdAt: new Date(),
+              updatedAt: new Date()
             },
             {
               startDate: "2021-6-16",
               endDate: "2021-6-21",
               userId: 1,
               spotId: 4,
-               
+              createdAt: new Date(),
+              updatedAt: new Date()
             },
             {
               startDate: "2021-5-20",
               endDate: "2021-5-26",
               userId: 1,
               spotId: 6,
-                
+              createdAt: new Date(),
+              updatedAt: new Date()
             },
         ], {});
     },
